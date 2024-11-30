dbe2679c58decc2b0c215846c08bf3d1c5362f67
lizzie992 <gulyaskata99@gmail.com>
2024-11-30 15:09:49 +0100

    removed the unnecessary Clearscreen methods

--- BB/Program.cs
+++ BB/Program.cs
@@ -11,8 +11,7 @@ namespace BB
             
             do
             {
-                //UserList = LogicalCode.LoadListFromFile();
-
+                UserList = LogicalCode.LoadListFromFile();
 
                 //main menu part:
                 answer = UserInterface.MainMenuSelection(answer);
@@ -24,33 +23,26 @@ namespace BB
                     UserInterface.WriteNewUserMessage();
                     User newUser = new User();
 
-                    UserInterface.ClearScreen();
-
                     newUser.EmailAddress = UserInterface.getEmail();
-                    UserInterface.ClearScreen();
                     newUser.FirstName = UserInterface.getFirstName();
-                    UserInterface.ClearScreen();
                     newUser.LastName = UserInterface.getLastName();
-                    UserInterface.ClearScreen();
 
                     UserInterface.printList(typeof(Department));
                     int numberOfDepartment = UserInterface.getDepartmentName();
                     newUser.Department = (Department)numberOfDepartment - 1;
-                    UserInterface.ClearScreen();
 
                     UserInterface.printList(typeof(PreferredLanguage));
                     int numberOfPreferredLanguage = UserInterface.getPreferrefLanguage();
                     newUser.PreferredLanguage = (PreferredLanguage)numberOfPreferredLanguage - 1;
-                    UserInterface.ClearScreen();
 
                     newUser.Contact = UserInterface.getContact();
-                    UserInterface.ClearScreen();
 
                     newUser.Rating = 0;
                     newUser.NumberOfWarnings = 0; //these are just basic setup for all users at the beginning:)
 
                     UserList.Add(newUser);
                     LogicalCode.SaveToFile(UserList);
+                    UserInterface.ClearScreen();
                 }
 
             } while (answer != Constants.SELECT_EXIT);
