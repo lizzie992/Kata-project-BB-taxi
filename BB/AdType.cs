de2dd53b224144928096a90e2518aff1753acdb0
The Programmer Princess <gulyaskata99@gmail.com>
2025-01-30 15:07:56 +0100

    Filtering function is finally working on the Ads page:)

--- BB/AdType.cs
+++ BB/AdType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,8 @@ namespace BB
 {
     public enum AdType
     {
-        Driver = 0,
-        Passenger
+        Driver,
+        Passenger,
+        SelectAll,
     }
 }
