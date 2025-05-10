a1edeb24be3dbd223cc66ea5aaac22798fae61db
The Programmer Princess <gulyaskata99@gmail.com>
2025-05-10 09:22:24 +0200

    reportAd method also moved to Dataservice

--- BaxiWebApp/BaxiWebApp/Data/DataService.cs
+++ BaxiWebApp/BaxiWebApp/Data/DataService.cs
@@ -100,6 +100,26 @@ namespace BaxiWebApp.Data
             }
         }
 
+        public void reportAd(Ad ad, User currentlyLoggedInUser)
+        {
+            string adDetails = $"Ad owner: {ad.AdOwner}\r\nAd type: {ad.AdType}\r\nRoute: {ad.Route}\r\nPick up date and time: {ad.pickUpDateAndTime}\r\nNumber of seats: {ad.NumberOfSeats}\r\nSpecific requests: {ad.SpecificRequests}\r\n";
+
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            client.EnableSsl = true;
+            client.UseDefaultCredentials = false;
+            string passw = Environment.GetEnvironmentVariable("BBtaxi_email_password");
+            client.Credentials = new NetworkCredential("baierbrunntaxi@gmail.com", passw);
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress("baierbrunntaxi@gmail.com");
+            mailMessage.To.Add("gulyaskata99@gmail.com");
+            mailMessage.Body = $"Please check out the following ad below that {currentlyLoggedInUser} reported to you: \r\n{adDetails}\r\nOpen the ad here: LINK";
+            mailMessage.Subject = "An ad was just reported to you";
+            client.Send(mailMessage);
+        }
+
+
+
+
         public void sendEmailNewAd(Ad ad)
         {
             string message = $"You might be interested in this new Ad:\r\n Ad type: {ad.AdType}\r\n In the following route: {ad.Route}\r\n On {ad.pickUpDateAndTime}\r\n Available seats: {ad.NumberOfSeats}\r\n Any speicifc requests: {ad.SpecificRequests}\r\n";
