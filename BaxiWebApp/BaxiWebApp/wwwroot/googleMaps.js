b1c1e1c65567b107445d940d2065481997d78ab8
The Programmer Princess <gulyaskata99@gmail.com>
2025-08-13 17:13:23 +0200

    automatic scroll to bottom now works:) (javascript file needs amendments)

--- BaxiWebApp/BaxiWebApp/wwwroot/googleMaps.js
+++ BaxiWebApp/BaxiWebApp/wwwroot/googleMaps.js
@@ -137,3 +137,13 @@ function getLongitude() {
     return longitude;
 }
 
+
+function scrollToBottom() {
+    const block = document.querySelector(".message-block");
+    block.scrollTop = block.scrollHeight;
+}
+
+function scrollToBottomPhone() {
+    const block = document.querySelector(".message-block-phone");
+    block.scrollTop = block.scrollHeight;
+}
