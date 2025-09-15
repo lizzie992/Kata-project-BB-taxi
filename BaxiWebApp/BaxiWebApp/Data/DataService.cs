2a1d32b11343495e52453ba89db90da15d6f66ab
The Programmer Princess <gulyaskata99@gmail.com>
2025-09-15 21:07:58 +0200

    safety commit

--- BaxiWebApp/BaxiWebApp/Data/DataService.cs
+++ BaxiWebApp/BaxiWebApp/Data/DataService.cs
@@ -601,7 +601,7 @@ namespace BaxiWebApp.Data
             client.EnableSsl = true;
             client.UseDefaultCredentials = false;
             string passw = Environment.GetEnvironmentVariable("BBtaxi_email_password");
-            client.Credentials = new NetworkCredential("baierbrunntaxi@gmail.com", passw);
+            client.Credentials = new NetworkCredential("baierbrunntaxi@gmail.com", "nilf kwog hanb kwtb");
             MailMessage mailMessage = new MailMessage();
             mailMessage.From = new MailAddress("baierbrunntaxi@gmail.com");
             mailMessage.To.Add(emailAddress);
